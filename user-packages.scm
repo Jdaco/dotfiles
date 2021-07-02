734b8baf877c19d28ccf003beacfe2a248e013c6
Chaise Conn <chaise.conn@protonmail.com>
2021-07-02 05:05:43 -0700

    Add manifest for user guix profile

---
+++ user-packages.scm
@@ -0,0 +1,69 @@
+(specifications->manifest
+  '(
+    ;;  Virtualization
+    "qemu"
+    "virt-manager"
+    "libvirt"
+    "dockercli"
+    ;;  Fonts
+    "font-linuxlibertine"
+    "font-liberation"
+    "font-ubuntu"
+    ;;  Disk Utilities
+    "gparted"
+    "nvme-cli"
+    "hdparm"
+    "parted"
+    "sdparm"
+    "gptfdisk"
+    ;; Monitoring
+    "iotop"
+    "sysstat"
+    "smartmontools"
+    "ncdu"
+    "acpi"
+    "lsof"
+    ;;  Media
+    "imagemagick"
+    "ffmpeg"
+    "pulsemixer"
+    "mpv"
+    "youtube-dl"
+    ;; Security
+    "pinentry-qt"
+    "password-store"
+    "gnupg"
+    "nmap"
+    ;; Xorg
+    "xclip"
+    "xdg-utils"
+    "xmodmap"
+    "alacritty"
+    ;; File utils
+    "rsync"
+    "zip"
+    "unzip"
+    "exa"
+    "ripgrep"
+    ;; Office
+    "mupdf"
+    "libreoffice"
+    "ispell"
+    "plantuml"
+    "scribus"
+    "offlineimap"
+    ;; Network
+    "nfs-utils"
+    "syncthing"
+    "curl"
+    "wget"
+    ;;
+    "stow"
+    "icecat"
+    "pandoc"
+    "awscli"
+    "scrot"
+    "ledger"
+    "jq"
+    "multipath-tools"
+    ))
