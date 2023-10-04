2021fcf28a9767386fcdb51fb1d0935fa5715cc0
Chaise Conn <chaise.conn@protonmail.com>
2023-10-04 08:48:03 -0700

    Add guix definition for k3d

--- guix/ccc.scm
+++ guix/ccc.scm
@@ -1,4 +1,5 @@
 (define-module (ccc)
+  #:use-module (guix build utils)
   #:use-module (guix git-download)
   #:use-module (gnu packages golang)
   #:use-module (gnu packages syncthing)
@@ -33,4 +34,36 @@
 with support for a multitude of operations including tab completion and wildcard
 support for files, which can be very handy for your object storage workflow
 while working with large number of files.")
+    (license expat)))
+
+(define-public k3d
+  (package
+    (name "k3d")
+    (version "5.6.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/k3d-io/k3d")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1p296igp61c5k4xzsb2zz0j4v3khhdpmsgfzrd81pip46fw311i8"))))
+    (build-system go-build-system)
+    (arguments
+     `(#:import-path "github.com/k3d-io/k3d/v5"
+      #:install-source? #f
+      #:phases
+        (modify-phases %standard-phases
+         (add-after 'install 'rename-binaries
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((out (assoc-ref outputs "out")))
+               (rename-file
+                     (string-append out "/bin/v5")
+                     (string-append out "/bin/k3d"))
+               #t))))
+           #:go ,go-1.19))
+    (home-page "https://github.com/k3d-io/k3d")
+    (synopsis " Little helper to run CNCF's k3s in Docker")
+    (description "k3d creates containerized k3s clusters. This means, that you can spin up a multi-node k3s cluster on a single machine using docker.")
     (license expat)))
