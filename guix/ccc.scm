838b4b947960fe7ac5ed02f662a4606d9a3848d4
Chaise Conn <chaise.conn@protonmail.com>
2023-10-06 23:45:36 -0700

    Add guix definition for s3fs

--- guix/ccc.scm
+++ guix/ccc.scm
@@ -1,14 +1,22 @@
 (define-module (ccc)
   #:use-module (guix build utils)
   #:use-module (guix git-download)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages golang)
   #:use-module (gnu packages syncthing)
   #:use-module (gnu packages databases)
   #:use-module (guix build-system go)
-  #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
   #:use-module (guix download))
+
+(import (only (guix licenses) expat))
 
 (define-public s5cmd
   (package
@@ -66,4 +74,32 @@ while working with large number of files.")
     (home-page "https://github.com/k3d-io/k3d")
     (synopsis " Little helper to run CNCF's k3s in Docker")
     (description "k3d creates containerized k3s clusters. This means, that you can spin up a multi-node k3s cluster on a single machine using docker.")
+    (license expat)))
+
+(define-public s3fs
+  (package
+    (name "s3fs")
+    (version "1.93")
+    (propagated-inputs (list libxml2 curl openssl awscli))
+    (inputs (list automake autoconf-2.71 pkg-config fuse-2))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/s3fs-fuse/s3fs-fuse")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "157g8w1kmr143b6spnzyhkxww3h1qjx3m90qznkk693j16fwgcpf"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:tests? ,#f
+       #:phases
+       (modify-phases %standard-phases
+                      (add-before 'configure 'autogen
+                                  (lambda* (#:key outputs #:allow-other-keys)
+                                    (invoke "./autogen.sh"))))))
+    (home-page "https://github.com/s3fs-fuse/s3fs-fuse")
+    (synopsis "FUSE-based file system backed by Amazon S3")
+    (description "FUSE-based file system backed by Amazon S3")
     (license expat)))
